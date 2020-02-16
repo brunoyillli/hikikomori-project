1408e13c3ffb77919d543ffc8ecf4656813c3b3b
brunoyillli <wolfteam1910@gmail.com>
2020-02-16 12:29:38 -0300

    voltando
    ok

--- Bullet Hell/scripts/bulets.gd
+++ Bullet Hell/scripts/bulets.gd
@@ -1,21 +1,14 @@
-extends Area2D
+extends KinematicBody2D
 
-var vel = 150
-var dir = Vector2(0,-1)
+var pos = Vector2()
 
 func _physics_process(delta):
-	translate(dir * vel * delta)
+	pos = Vector2()
+	pos.y -= 850
+	pos = move_and_slide(pos)
 	
 func _ready():
-	add_to_group("tiro_basico")
 	pass 
 
 func _on_Timer_timeout():
-	queue_free()
-	
-func _on_ship_shot_area_enter(area):
-	if area.has_method("destroy"):
-		area.destroy(self)
-		
-func destroy():
 	queue_free()
