3874b103a0b6130d426dd2033acfae697d320a22
brunoyillli <wolfteam1910@gmail.com>
2020-02-16 11:16:17 -0300

    Adicionado conceito de varias inimigos raposas
    o TIPO de inimigo A, B e C é marcado pela propria enginner, por enquanto colocar 3 copias do inimigo e marcar o tipo de cada 1 e então vai aparecer varios diferentes.

--- Bullet Hell/Kitsune_menor.gd
+++ Bullet Hell/Kitsune_menor.gd
@@ -1,11 +1,30 @@
 extends Area2D
 
 var vel = 150
-var y = 1
-var x = 1	
+export(int, "A", "B", "C") var tipo = 0
+var score = 0
+var atributos =[
+	{
+		texture = preload("res://Sprites/kitsune_medio.png"),
+		score = 10
+	},
+	{
+		texture = preload("res://Sprites/kitsune_menor.png"),
+		score = 30
+	},
+	{
+		texture = preload("res://Sprites/kitsune_baixo.png"),
+		score = 50
+	}
+]
+
+
 func _ready():
-	randomize()
-	pass # Replace with function body.
+	var atributo = atributos[tipo]
+	get_node("Sprite").set_texture(atributo.texture)
+	score = atributo.score
+	
+	pass 
 
 func _physics_process(delta):
 	set_position(get_position() + Vector2(0,1) * vel * delta)
